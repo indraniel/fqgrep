f148f32f5ca3a5bf1b6d5809453fb1c85a197b0c
Indraniel Das <indraniel@gmail.com>
2010-10-25 00:51:36 -0500

    + first-pass attempt to use the libbitap library
      - replace the wikipedia bitap implementation with libbitap
        (http://rational.co.za/libbitap)
    
        libbitap handles the cases for insertions, deletions, and
        substitutions unlike the wikipedia version (which only dealt
        with substitutions).
    
        libbitap also some limited amounts of regular expression
        syntax.  Although its usage is not well documented.

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 .PHONY: clean
 
-fqgrep: fqgrep.o bitap.o bm.o
-	gcc -g -lz -o fqgrep fqgrep.o bitap.o bm.o
+fqgrep: fqgrep.o libbitap.o bm.o
+	gcc -g -lz -o fqgrep fqgrep.o libbitap.o bm.o
 
 fqgrep.o: fqgrep.c kseq.h
 	gcc -g -I. -c fqgrep.c
 
-bitap.o: bitap.c bitap.h
-	gcc -g -I. -c bitap.c
+libbitap.o: libbitap.c libbitap.h
+	gcc -g -I. -c libbitap.c
 
 bm.o: bm.c bm.h
 	gcc -g -I. -c bm.c
--- fqgrep.c
+++ fqgrep.c
@@ -6,10 +6,10 @@
 #include <zlib.h>  
 #include "kseq.h"
 #include "bm.h"
-#include "bitap.h"
+#include "libbitap.h"
 
 /* D E F I N E S *************************************************************/
-#define VERSION "0.1"
+#define VERSION "0.2"
 #define PRG_NAME "fqgrep"
 #define FASTQ_FILENAME_MAX_LENGTH 1024
 #define MAX_PATTERN_LENGTH 1024
@@ -18,29 +18,33 @@
 typedef struct {
     int count;
     int color;
+    int force_bitap;
     int mismatches;
     char output_fastq[FASTQ_FILENAME_MAX_LENGTH];
     char search_pattern[MAX_PATTERN_LENGTH];
+    bitapType *bitap_regexp;  // compiled libbitap regexp
 } options;
 
 /* 
-   declare the type of file handler and the read() function  
+   declare the type of file handler and the read() function
    as described here:
    http://lh3lh3.users.sourceforge.net/parsefastq.shtml
 */
 KSEQ_INIT(gzFile, gzread)  
 
 /* P R O T O T Y P E S *******************************************************/
-void help_message(void);
-void version_info(void);
-int  process_options(int argc, char *argv[], options *opts);
-void search_input_fastq_file(FILE *out_fp,
-                             const char *input_fastq, 
-                             const options opts);
-void display_read(FILE *out_fp, 
-                  const kseq_t *seq, 
-                  const char *substr_position_start, 
-                  const options opts);
+void  help_message(void);
+void  version_info(void);
+int   process_options(int argc, char *argv[], options *opts);
+void  search_input_fastq_file(FILE *out_fp,
+                              const char *input_fastq,
+                              const options opts);
+void  display_read(FILE *out_fp,
+                   const kseq_t *seq,
+                   const char *substr_position_start,
+                   const char *substr_position_end,
+                   const options opts);
+void  compile_bitap_regexp(bitapType *b, options *opts);
 char* substring(const char *str, size_t start, size_t len);
 char* stringn_duplicate(const char *str, size_t n);
 
@@ -53,7 +57,7 @@ int main(int argc, char *argv[]) {
     int opt_idx, index;
     FILE *out_fp;
     char input_fastq[FASTQ_FILENAME_MAX_LENGTH] = { '\0' };
-    options opts = { 0, 0, 0, {'\0'}, {'\0'} };
+    options opts = { 0, 0, 0, 0, {'\0'}, {'\0'}, NULL };
 
     opt_idx = process_options(argc, argv, &opts);
 
@@ -61,6 +65,12 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "%s : %s\n",
                         PRG_NAME, "[err] specify FASTQ files to process!");
         exit(1);
+    }
+
+    /* compile bitap regexp if needed */
+    if (opts.mismatches != 0 || opts.force_bitap == 1) {
+        bitapType bitap;
+        compile_bitap_regexp( &bitap, &opts );
     }
 
     /* setup the appropriate output file pointer */
@@ -98,6 +108,7 @@ help_message() {
     fprintf(stdout, "\t%-20s%-20s\n", "-c", "Highlight matching string with color");
     fprintf(stdout, "\t%-20s%-20s\n", "-m", "Number of mismatches to allow for search pattern");
     fprintf(stdout, "\t%-20s%-20s\n", "", "[Default: 0]");
+    fprintf(stdout, "\t%-20s%-20s\n", "-b", "Force bitap algorithm usage");
     fprintf(stdout, "\t%-20s%-20s\n", "-C", "Display only a total count of matches");
     fprintf(stdout, "\t%-20s%-20s\n", "", "(per input FASTQ file)");
     fprintf(stdout, "\t%-20s%-20s\n", "-o <fastq_file>", "Desired fastq output file.");
@@ -116,7 +127,7 @@ process_options(int argc, char *argv[], options *opts) {
     char *opt_o_value = NULL;
     char *opt_p_value = NULL;
 
-    while( (c = getopt(argc, argv, "hvcm:o:p:C")) != -1 ) {
+    while( (c = getopt(argc, argv, "hvbcm:o:p:C")) != -1 ) {
         switch(c) {
             case 'h':
                 help_message();
@@ -125,6 +136,9 @@ process_options(int argc, char *argv[], options *opts) {
             case 'v':
                 version_info();
                 exit(0);
+                break;
+            case 'b':
+                opts->force_bitap = 1;
                 break;
             case 'o':
                 opt_o_value = optarg;
@@ -184,28 +198,30 @@ process_options(int argc, char *argv[], options *opts) {
 
 void
 search_input_fastq_file(FILE *out_fp, 
-                        const char *input_fastq, 
+                        const char *input_fastq,
                         const options opts) {
-    gzFile fp;  
-    kseq_t *seq;  
+    gzFile fp;
+    kseq_t *seq;
     int l, match_counter = 0;
-    char *substr_position_start;
+    char *substr_position_start = NULL;
+    char *substr_position_end = NULL;
+    int actual_mismatches = 0;
 
-    // open the file handler  
+    // open the file handler
     if ( (fp = gzopen(input_fastq, "r")) == NULL ) {
         fprintf(stderr, "%s : [err] Could not open FASTQ '%s' for reading.\n",
                         PRG_NAME, input_fastq);
         exit(1);
     } 
 
-    // initialize seq  
-    seq = kseq_init(fp); 
+    // initialize seq
+    seq = kseq_init(fp);
 
     // read sequence  
-    while ( (l = kseq_read(seq)) >= 0 ) { 
+    while ( (l = kseq_read(seq)) >= 0 ) {
         if ( strlen(seq->seq.s) < strlen(opts.search_pattern) ) {
             fprintf(stderr, "%s : %s '%s' %s (%zd) %s (%zd).\n",
-                            PRG_NAME, 
+                            PRG_NAME,
                             "[err] For sequence ",
                             seq->name.s,
                             "search pattern length",
@@ -215,23 +231,42 @@ search_input_fastq_file(FILE *out_fp,
             exit(1);
         }
 
-        if (opts.mismatches == 0) {
+        if (opts.mismatches == 0 && opts.force_bitap == 0) {
+//            fprintf(stdout, "Running boyer moore search\n");
             substr_position_start = 
                 (char *) boyermoore_search( seq->seq.s, opts.search_pattern );
+            if (substr_position_start != NULL) {
+                substr_position_end =
+                    substr_position_start + strlen(opts.search_pattern);
+            }
         }
         else {
-            substr_position_start = 
-                (char *) bitap_fuzzy_bitwise_search(
-                        seq->seq.s, 
-                        opts.search_pattern, 
-                        opts.mismatches 
+//            fprintf(stdout, "Running libbitap search\n");
+            substr_position_end =
+                 FindWithBitap(
+                        opts.bitap_regexp,
+                        seq->seq.s,
+                        strlen(seq->seq.s),
+                        opts.mismatches,
+                        &actual_mismatches,
+                        &substr_position_start
                 );
+//            fprintf(stdout, "substr_position_end : %p (%s) \n", substr_position_end, substr_position_end);
+//            fprintf(stdout, "substr_position_start : %p (%s) \n", substr_position_start, substr_position_start);
+//            fprintf(stdout, "mismatches : %d\n", opts.mismatches);
+//            fprintf(stdout, "acutal mismatches : %d\n", actual_mismatches);
         }
 
         if (substr_position_start != NULL) {
             match_counter++;
             if (opts.count == 0)
-                display_read(out_fp, seq, substr_position_start, opts);
+                display_read(
+                        out_fp,
+                        seq,
+                        substr_position_start,
+                        substr_position_end,
+                        opts
+                );
         }
     }
 
@@ -252,7 +287,8 @@ search_input_fastq_file(FILE *out_fp,
 void 
 display_read(FILE *out_fp, 
              const kseq_t *seq, 
-             const char *substr_position_start, 
+             const char *substr_position_start,
+             const char *substr_position_end,
              const options opts) {
 
     /* header portion of FASTQ read record */
@@ -265,12 +301,13 @@ display_read(FILE *out_fp,
 
         if (sequence == substr_position_start) {
             char *highlight = 
-                substring( sequence, 0, strlen(opts.search_pattern) );
+                substring( sequence, 0, substr_position_end - sequence );
 
-            start  = strlen(opts.search_pattern);
-            length = strlen(sequence) - strlen(opts.search_pattern);
+            start  = substr_position_end - substr_position_start;
+            length = strlen(sequence) -
+                     ( substr_position_end - substr_position_start );
 
-            char *remainder = 
+            char *remainder =
                 substring( sequence, start, length );
 
             fprintf(out_fp, "\033[31m%s\033[0m%s\n", highlight, remainder);
@@ -283,7 +320,7 @@ display_read(FILE *out_fp,
             char *begin = substring( sequence, start, length ); 
 
             start  = start + length;
-            length = strlen(opts.search_pattern);
+            length = substr_position_end - substr_position_start;
             char *highlight = substring( sequence, start, length );
 
             start  = start + length;
@@ -317,6 +354,57 @@ display_read(FILE *out_fp,
     }
 }
 
+void compile_bitap_regexp(bitapType *b, options *opts) {
+    size_t search_pattern_length;
+    char *bitap_search_pattern;
+
+    // We are implicitly looking for substring matches.  We do not
+    // want to match against the whole input read. According to
+    // http://rational.co.za/libbitap/ we must prefix and postfix the
+    // search pattern with '.*' for this to occur properly.
+
+    search_pattern_length = strlen(".*")
+                            + strlen(opts->search_pattern)
+                            + strlen(".*");
+
+    // account for NULL termination
+    bitap_search_pattern = malloc(search_pattern_length + 1);
+    if ( bitap_search_pattern == NULL ) {
+        fprintf(stderr, "%s : %s %s\n",
+                        PRG_NAME,
+                        "Trouble mallocing for bitap search pattern string.",
+                        "Out of memory!");
+        exit(1);
+    }
+
+    // initialize the string to NULLs
+    memset(bitap_search_pattern, '\0', search_pattern_length + 1);
+
+    // construct the appropriate bitap search pattern string
+    strcat(bitap_search_pattern, ".*");
+    strcat(bitap_search_pattern, opts->search_pattern);
+    strcat(bitap_search_pattern, ".*");
+
+//    fprintf(stdout, "bitap_search_pattern is: |%s|\n", bitap_search_pattern);
+
+    // compile the bitap search string according to the libbitap protocol
+    int result = NewBitap(b, bitap_search_pattern);
+    if (result < 0) {
+        fprintf(stderr, "%s : %s '%s' %s %d\n",
+                        PRG_NAME,
+                        "[err] Trouble compiling bitap search pattern",
+                        opts->search_pattern,
+                        "at character position ",
+                        (-1) * result );
+    }
+
+    // assign it to our options data structure
+    opts->bitap_regexp = b;
+
+    // free the search string
+    free(bitap_search_pattern);
+}
+
 char* 
 substring(const char *str, size_t start, size_t len) {
     char *substr;
@@ -332,10 +420,10 @@ substring(const char *str, size_t start, size_t len) {
 }
 
 /*
-   'stringn_duplicate' is really a poor man's duplication of GNU/libc's
-   'strndup'. However not all types of UNIXes implement strndup (Apple,
-   FreeBSD). So the function below is used instead for the ease of
-   portability.
+   'stringn_duplicate' is really a poor man's duplication of glibc's
+   'strndup'. However not all types of UNIXes implement strndup (like
+   Apple, & FreeBSD). So the function below is used instead for the ease
+   of portability.
 
    The implementation is taken from :
      http://webmail.solomo.de/~flo/strndup.patch
